74b6353c2cb97ac38d8fd8ca1fab46108712bbca
Carlos5682 <samuelbarreno10@gmail.com>
2025-06-28 15:08:00 +0200

    subiendo carpeta de scripts

---
+++ Scripts/Corine/Corine.R
@@ -0,0 +1,15 @@
+rm(list=ls())
+library(ggrepel)
+library(ggspatial)
+library(mapSpain)
+library(sf)
+library(dplyr)
+library(ggspatial)
+library(ggplot2)
+library(future.apply)
+setwd("C://Users//samue//Desktop//proyectotfg")
+
+
+
+
+
